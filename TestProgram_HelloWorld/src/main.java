2287c962d4b75cbf382063c12e89efa6253db075
nalhamal-CSUEB <nalhamal@horizon.csueastbay.edu>
2023-04-19 16:22:45 -0700

    Added new line to print "First New Line"

--- TestProgram_HelloWorld/src/main.java
+++ TestProgram_HelloWorld/src/main.java
@@ -3,6 +3,7 @@ public class main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World!");
+		System.out.println("First New Line");
 		
 		System.exit(0);
 	}
