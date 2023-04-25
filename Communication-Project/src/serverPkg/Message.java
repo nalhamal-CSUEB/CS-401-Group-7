f734e6cf11d1ec0d52b4166d88c3e00891afec35
workcomputer <workcomputer@localhost>
2023-04-25 16:54:05 -0700

    Message class is completed, needs confirmation of team. Message class has a constructor and the getters for the function.

--- Communication-Project/src/serverPkg/Message.java
+++ Communication-Project/src/serverPkg/Message.java
@@ -1,5 +1,42 @@
 package serverPkg;
 
-public class Message {
+import java.io.Serializable;
+import java.util.Date;
 
+
+public class Message implements Serializable {
+	protected String fromAcctNum;
+    protected String destinationID;
+    protected String message;
+    protected Date date;
+    protected MsgType msgType;
+
+    // constructor
+    public Message(String fromAcctNum, String destinationID, String message, MsgType msgType){
+        this.fromAcctNum = fromAcctNum;
+        this.destinationID = destinationID;
+        this.msgType = msgType;
+        this.message = message;
+    }
+	
+	// getters
+    public String getFrom(){
+    	return fromAcctNum;
+    }
+
+    public String getTo(){
+    	return destinationID;
+    }
+
+    public MsgType getMsgType(){
+    	return msgType;
+    }
+    
+    public String getMessage() {
+    	return message;
+    }
+
+    public Date getDate() {
+    	return date;
+    }
 }
