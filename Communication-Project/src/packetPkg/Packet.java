d65c4ced598644bc1976f636f3dfbcc3616daabb
LucasAudero <laudero@horizon.csueastbay.edu>
2023-04-26 13:28:24 -0700

    Completed Packet class

--- Communication-Project/src/packetPkg/Packet.java
+++ Communication-Project/src/packetPkg/Packet.java
@@ -1,5 +1,26 @@
 package packetPkg;
-
+import serverPkg.Message;
 public class Packet {
-
+	
+	protected PacketType type;
+	protected RequestType request;
+	protected Message message;
+	
+	public Packet(PacketType type, RequestType request, Message message) {
+		this.type = type;
+		this.request = request;
+		this.message = message;
+	}
+	
+	PacketType getPacketType() {
+		return type;
+	}
+	
+	RequestType getRequestType() {
+		return request;
+	}
+	
+	Message getMessage() {
+		return message;
+	}
 }
