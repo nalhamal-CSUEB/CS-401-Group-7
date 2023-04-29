b55a5e13a84152613ced289063b9686f91c444ea
omidn <omid.naqshbandi786@gmail.com>
2023-04-29 16:20:11 -0700

    ENUMS DONE!

--- Communication-Project/src/packetPkg/RequestType.java
+++ Communication-Project/src/packetPkg/RequestType.java
@@ -1,5 +1,13 @@
 package packetPkg;
 
 public enum RequestType {
-
+	SEND_MESSAGE,    //Send Message Request
+	RECEIVE_MESSAGE, //Receive Message Request
+	CREATE_GROUP,	 //Create Group Request
+	CREATE_CHAT,	 //Create Chat Request
+	JOIN_GROUP,		 //Join Group Request
+	LEAVE_GROUP,	 //Leave Group Request
+	KICK_USER,		 //Kick User Request
+	REPORT_USER,	 //Report User Request
+	BLOCK_USER		 //Block User Request
 }
--- Communication-Project/src/serverPkg/MsgType.java
+++ Communication-Project/src/serverPkg/MsgType.java
@@ -1,5 +1,6 @@
 package serverPkg;
 
 public enum MsgType {
-
+	CHAT, //Message in 1-1 chat 
+	Group //Message in a group chat
 }
