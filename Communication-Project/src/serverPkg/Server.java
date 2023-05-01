885ece6c58d895cf9e2adf7d7a70330fb84023d1
LucasAudero <laudero@horizon.csueastbay.edu>
2023-05-01 10:46:30 -0700

    Started multi-threaded Server Made most of System

--- Communication-Project/src/serverPkg/Server.java
+++ Communication-Project/src/serverPkg/Server.java
@@ -1,10 +1,94 @@
 package serverPkg;
-
+import java.io.*;
+import java.net.*;
 public class Server {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		ServerSocket server = null;
 
+		try {
+
+			// server is listening on port 1234
+			server = new ServerSocket(1234);
+			server.setReuseAddress(true);
+
+			// running infinite loop for getting
+			// client request
+			while (true) {
+
+				// socket object to receive incoming client
+				// requests
+				Socket client = server.accept();
+
+				// Displaying that new client is connected
+				// to server
+				System.out.println("New client connected"
+								+ client.getInetAddress()
+										.getHostAddress());
+
+				// create a new thread object
+				ClientHandler clientSock
+					= new ClientHandler(client);
+
+				// This thread will handle the client
+				// separately
+				new Thread(clientSock).start();
+			}
+		}
+		catch (IOException e) {
+			e.printStackTrace();
+		}
+		finally {
+			if (server != null) {
+				try {
+					server.close();
+				}
+				catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		
+		// ClientHandler class
+		private static class ClientHandler implements Runnable {
+			private final Socket clientSocket;
+		    private ObjectOutputStream out;
+		    private ObjectInputStream in;
+			// Constructor
+			public ClientHandler(Socket socket) throws IOException
+			{
+				this.clientSocket = socket;
+		        out = new ObjectOutputStream(clientSocket.getOutputStream());
+		        in = new ObjectInputStream(clientSocket.getInputStream());
+			}
+
+			public void run()
+			{
+		        try {
+		            Message message = (Message) in.readObject();
+		            if (message.getType().equals("login")) {
+		                message.setStatus("success");
+		                out.writeObject(message);
+		                out.flush();
+		                while (true) {
+		                    message = (Message) in.readObject();
+		                    if (message.getType().equals("text")) {
+		                        message.setText(message.getText());
+		                        out.writeObject(message);
+		                        out.flush();
+		                    } else if (message.getType().equals("logout")) {
+		                        message.setStatus("success");
+		                        out.writeObject(message);
+		                        out.flush();
+		                        clientSocket.close();
+		                        break;
+		                    }
+		                }
+		            }
+		        } catch (IOException | ClassNotFoundException e) {
+		        	e.printStackTrace();
+		        }
+			}
+		}
 	}
-
 }
--- Communication-Project/src/serverPkg/System.java
+++ Communication-Project/src/serverPkg/System.java
@@ -1,5 +1,135 @@
 package serverPkg;
-
+import java.util.ArrayList;
 public class System {
-
+	private ArrayList<GeneralUser> generalUser;
+	private ArrayList<ITUser> itUser;
+	private ArrayList<User> connectedUser;
+	private ArrayList<Chat> chats;
+	private ArrayList<Group> publicGroups;
+	private ArrayList<Group> privateGroups;
+	private ArrayList<Group> deletedGroups;
+	
+	public System() {
+		this.generalUser = new ArrayList<GeneralUser>();
+		this.itUser = new ArrayList<ITUser>();
+		this.connectedUser = new ArrayList<User>();
+		this.chats = new ArrayList<Chat>();
+		this.publicGroups = new ArrayList<Group>();
+		this.privateGroups = new ArrayList<Group>();
+		this.deletedGroups = new ArrayList<Group>();
+	}
+	
+	public bool login(User user) {
+		return true;
+	}
+	
+	public bool verify(User user) {
+		return true;
+	}
+	
+	public bool logout() {
+		return true;
+	}
+	
+	//connected user methods
+	public void addConnectedUser(User user) {
+		connectedUser.add(user);
+	}
+	
+	public void removeConnectedUser(User user) {
+		for (int i = 0; i < connectedUser.size(); i++) {
+		      if (connectedUser.password.equals(user.password)) {
+		    	  connectedUser.remove(i);
+		      }
+		}
+	}
+	
+	public ArrayList<User> getConnectedUsers() {
+		return connectedUser;
+	}
+	
+	//general user methods
+	public void addGeneralUser(GeneralUser user) {
+		generalUser.add(user);
+	}
+	
+	public void removeGeneralUser(User user) {
+		//no delete account
+	}
+	
+	public ArrayList<User> getGeneralUsers() {
+		return generalUser;
+	}
+	
+	//it user methods
+	public void addITUser(User user) {
+		itUser.add(user);
+	}
+	
+	public void removeITUser(User user) {
+		//no delete account
+	}
+	
+	public ArrayList<User> getITUsers() {
+		return itUser;
+	}
+	
+	//group methods
+	public void addGroup(Group group) {
+		
+	}
+	
+	public void removeGroup(Group group) {
+		//checks for private or public then copies group to deleted groups while
+		//removing from visable groups
+		if (group.isPrivate == true) {
+			for (int i = 0; i < privateGroups.size(); i++) {
+			      if (privateGroups.getGroupID().equals(group.getGroupID())) {
+			    	  deletedGroups.add(privateGroups[i]);
+			    	  privateGroups.remvoe(i);
+			      }
+			}
+		}
+		else {
+			for (int i = 0; i < publicGroups.size(); i++) {
+			      if (publicGroups.getGroupID().equals(group.getGroupID())) {
+			    	  deletedGroups.add(publicGroups[i]);
+			    	  publicGroups.remvoe(i);
+			      }
+			}
+		}
+	}
+	
+	public ArrayList<Group> getPublicGroups() {
+		return publicGroups;
+	}
+	
+	public ArrayList<Group> getPrivateGroups() {
+		return privateGroups;
+	}
+	
+	public ArrayList<Group> getDeletedGroups() {
+		return deletedGroups;
+	}
+	
+	public void writeToGroup() {
+		
+	}
+	
+	public Packet readChat() {
+		
+	}
+	
+	//chat methods
+	public void addChat(Chat chat) {
+		chats.add(chat);
+	}
+	
+	public void writeToChat() {
+		
+	}
+	
+	public Packet readChat() {
+		
+	}
 }
